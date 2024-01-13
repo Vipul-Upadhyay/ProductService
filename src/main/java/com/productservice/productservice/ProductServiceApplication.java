09b353ed532da67f8af8480fc8a23818f4ac0b9b
Vipul-Upadhyay <upa.vipul@gmail.com>
2024-01-13 11:01:16 +0530

    DB versioning

--- src/main/java/com/productservice/productservice/ProductServiceApplication.java
+++ src/main/java/com/productservice/productservice/ProductServiceApplication.java
@@ -1,13 +1,45 @@
 package com.productservice.productservice;
 
+import com.productservice.productservice.repositories.CategoryRepository;
+import com.productservice.productservice.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductServiceApplication {
+public class ProductServiceApplication implements CommandLineRunner {
+
+    private final CategoryRepository categoryRepository;
+    private final ProductRepository productRepository;
+
+    public ProductServiceApplication(CategoryRepository categoryRepository,
+                                     ProductRepository productRepository) {
+        this.categoryRepository = categoryRepository;
+        this.productRepository = productRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
+
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+//        Category category = new Category();
+//        category.setName("Apple Devices");
+//        Category savedCategory = categoryRepository.save(category);
+//
+//        Price price = new Price();
+//        price.setValue(100000);
+//        price.setCurrency("INR");
+//
+//        Product product = new Product();
+//        product.setPrice(price);
+//        product.setTitle("iPhone 15 pro");
+//        product.setDescription("Best iPhone ever");
+//        product.setImage("img");
+//        product.setCategory(savedCategory);
+//        Product savedProduct = productRepository.save(product);
+    }
 }
--- src/main/java/com/productservice/productservice/models/Product.java
+++ src/main/java/com/productservice/productservice/models/Product.java
@@ -1,19 +1,25 @@
 package com.productservice.productservice.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product  extends BaseModel{
 
     private String title;
     private String description;
-    private int price;
     private String image;
+
     @ManyToOne
     private Category category;
+
+    @OneToOne(cascade = {CascadeType.REMOVE, CascadeType.PERSIST})
+    private Price price;
 }
